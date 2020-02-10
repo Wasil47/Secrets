3a498a098977913ab581473b64d28cfdcb042986
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-02-10 18:25:09 +0100

    add hashing, delete encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -18,9 +18,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-//const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] }); // Before new mongoose.model (userSchema)
 
 
 const User = new mongoose.model('User', userSchema); //User => collection 'users'
@@ -40,7 +37,7 @@ app.get('/register', (req, res)=>{
 app.post('/register', (req, res)=>{
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save((err)=>{
     if (err)
@@ -51,7 +48,7 @@ app.post('/register', (req, res)=>{
 
 app.post('/login', (req, res)=>{
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser)=>{
     if (err){
