c2e05c3aec614d29dea71311301f9e7c29e96868
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-02-10 16:32:27 +0100

    add password encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -12,11 +13,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-// * = new mongoose.Schema({}) ??
-const userSchema = { 
+const userSchema = new mongoose.Schema({ 
   email: String,
   password: String
-};
+});
+
+//const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] }); // Before new mongoose.model (userSchema)
+
+
 const User = new mongoose.model('User', userSchema); //User => collection 'users'
 
 // (req, res)=>{}
